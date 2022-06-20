d0822ee1589998faf3f548ea15ef3e23374e82ad
Ibrahima Khaliloulaye DIALLO <58261204+dialloibrahimakhaliloulaye@users.noreply.github.com>
2022-06-20 04:18:39 +0000

    define routes and classes font

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ClasseComponent} from "./classe/classe.component";
+import {EleveComponent} from "./eleve/eleve.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "classes", component: ClasseComponent},
+  {path: "eleves", component: EleveComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/classe/classe.component.ts
+++ src/app/classe/classe.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClasseComponent implements OnInit {
 
+  classes: Array<any> | undefined;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.classes=[
+      {id:1, niveau:"terminale", index:"A", nbreEleve:55},
+      {id:2, niveau:"terminale", index:"B", nbreEleve:40},
+      {id:3, niveau:"premiere", index:"C", nbreEleve:60},
+      {id:4, niveau:"premiere", index:"A", nbreEleve:50}
+    ];
   }
 
+  deleteClasse(c:any) {
+    
+  }
 }
